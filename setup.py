520dd4c6ccf78f36a0ce75560c459b0d7dd1f93f
Sinesipo <48671920+Sinesipo@users.noreply.github.com>
2019-03-19 15:38:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='package_root',
+    name='package',
     version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/Sinesipo/package_root.git',
+    url='https://github.com/Sinesipo/package.git',
     author='Sinesipo',
     author_email='smbasa1818@gmail.com'
 )
